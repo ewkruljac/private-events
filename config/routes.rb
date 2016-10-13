e2442758355566d4c29127e737b711b803ccc6a6
Edward Kruljac <ewkruljac@gmail.com>
2016-10-13 01:41:38 -0600

    Log in and Log out added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/home', to: 'static_pages#home'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
