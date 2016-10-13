089c1d5353bf727120ed4c09a87369057a7da072
Edward Kruljac <ewkruljac@gmail.com>
2016-10-13 00:28:26 -0600

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
   end
+
+#----------
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+#----------
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to EventJack!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+#----------
+
+  def index
+    @users = User.all
+  end
+
+
+#----------    PRIVATE METHODS    ----------
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
+  root 'static_pages#home'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/home', to: 'static_pages#home'
 
-  root 'static_pages#home'
+  resources :users
 end
